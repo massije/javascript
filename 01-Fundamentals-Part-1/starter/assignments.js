9b1fcf89330a881878250169625657c559928164
massije <jedc.marti@gmail.com>
2024-05-10 23:21:57 +0200

    Assignment-1-2-3-finished

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,47 @@
+console.log("\n\n--------------------------------------------------------------");
+// First assignmet
+console.log("01-Assignment")
+
+/*1.- Declare variables called country, continent and population and assign their values according to your own country (population in millions).*/
+let countryy;
+let continentt;
+let population;
+
+countryy = "Spain";
+continentt = "Europe";
+population = 10;
+
+/*2.- Log their values to the console.*/
+console.log("These are my details\n")
+console.log("Country: " + countryy);
+console.log("Continent: " + continentt);
+console.log("Population: " + population);
+
+// Second Assignment
+console.log("\n\n02-Assignment");
+
+/* 1.- Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet. */
+let isIslandd = false;
+let language;
+
+/* 2.- Log the types of isIsland, population, country and language to the console. */
+
+console.log("Tipo de la variable isIsland: " + typeof isIslandd);
+console.log("Tipo de la varaible population: " + typeof population)
+console.log("Tipo de la variable country: " + typeof countryy);
+console.log("Tipo de la variable language: " + typeof language);
+
+// Third Assignment
+console.log("\n\n03-Assignment");
+
+/* 1.- Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one). */
+language = "Spanish";
+
+/* 2.- Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const. */
+const country = "Spain";
+const continent = "Europe";
+const isIsland = false;
+
+/* 3.- Try to change one of the changed variables now, and observe what happens. */
+// countryy = "Peru";
+console.log(countryy);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,35 @@
+/*let js = 'amazing';
+console.log(40 + 50 + 10)
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Jorge Martin Del Pino Contreras";
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+
+console.log("Hello every one, my name is " + firstName + " and I work as a " + myFirstJob);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "jorge");
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);
+
+let age = 26;
+age = 27;
+
+const birthYear = 1997;
+birthYear = 1998;
+*/
