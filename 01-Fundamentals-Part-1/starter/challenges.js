fba6c8d2f2855817359b3aa4f04a9ce6a8c55684
massije <jedc.marti@gmail.com>
2024-07-06 07:12:41 +0200

    Challenge-03-finished

--- 01-Fundamentals-Part-1/starter/challenges.js
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -15,3 +15,49 @@ const markHigherBMI = BMIMark > BMIJohn;
 
 console.log(BMIMark, BMIJohn);
 console.log(markHigherBMI);
+
+console.log(
+  "\n\n--------------------------------------------------------------"
+);
+// Second Challenge
+console.log("02-Challenge");
+let nameWin, nameLose;
+if (BMIMark > BMIJohn) {
+  nameWin = "Mark";
+  nameLose = "John";
+} else {
+  nameWin = "John";
+  nameLose = "Mark";
+}
+
+const outputMessage = `${nameWin}'s BMI is higher than ${nameLose}'s!`;
+console.log(outputMessage);
+
+console.log(
+  "\n\n--------------------------------------------------------------"
+);
+// Third Challenge
+console.log("03-Challenge");
+const numberOfGames = 3;
+
+const scoreGame1Dolphins = 96;
+const scoreGame2Dolphins = 108;
+const scoreGame3Dolphins = 89;
+
+const scoreGame1Koalas = 88;
+const scoreGame2Koalas = 91;
+const scoreGame3Koalas = 110;
+
+const scoreDolphins =
+  (scoreGame1Dolphins + scoreGame2Dolphins + scoreGame3Dolphins) /
+  numberOfGames;
+const scoreKoalas =
+  (scoreGame1Koalas + scoreGame2Koalas + scoreGame3Koalas) / numberOfGames;
+
+if (scoreDolphins === scoreKoalas) {
+  console.log("Both win the trophy");
+} else if (scoreDolphins > scoreKoalas) {
+  console.log("Dolphins win the trophy");
+} else {
+  console.log("Koalas win the trophy");
+}
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -71,4 +71,128 @@ console.log(ageJonas > ageSarah);
 const isFullAge = ageSarah >= 18;
 console.log(isFullAge);
 
+
+const firstName = "Jorge Martin";
+const job = "student";
+const birthYear = 1997;
+const currentYear = 2024;
+
+const jorge =
+  "I'm " +
+  firstName +
+  ", a " +
+  job +
+  " and i was born in " +
+  birthYear +
+  " then " +
+  "I'm " +
+  (currentYear - birthYear) +
+  " year old";
+
+console.log(jorge);
+
+const jorgeNew = `I'm ${firstName}, a ${job} and I was born in ${birthYear} then I'm ${
+  currentYear - birthYear
+} year old!`;
+console.log(jorgeNew);
+
+console.log("String with\nmultiple\nlines");
+console.log(`String with
+multiple
+lines`);
+
+
+const age = 17;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) {
+  console.log(`The person is old!`);
+} else {
+  const yearLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearLeft}!`);
+}
+
+const birthYear = 1997;
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(`The century is ${century}`);
+
+
+// type conversion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 2, inputYear + 2);
+console.log(Number("Jorge"));
+console.log(typeof NaN);
+console.log(String(23), 23);
+console.log(typeof String(23), typeof 12);
+
+// type coercion
+console.log("I am " + 23 + " years old");
+console.log("23" + "10" + 12);
+console.log("23" - "10" - 3);
+
+let n = "1" + 1;
+n = n - 1;
+console.log(n);
+
+let b = 1 + "1";
+console.log(b);
+--b;
+console.log(b);
+
+
+// 5 falsy values: 0, "", undefined, null NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("jorge"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+let money;
+if (money) {
+  console.log("Don't spend it all ;)");
+} else {
+  console.log("You should get a job!");
+}
+
+const age = 18;
+if (age === 18) console.log("You are old! ;)");
+else console.log("You are young ;(");
+
+const miNumber = Number(prompt("What's your favourite number?"));
+console.log(miNumber);
+console.log(typeof miNumber);
+console.log(typeof Number(miNumber));
+
+if (miNumber === 23) console.log("This number is amazing!");
+else if (miNumber === 7) console.log("7 is an amazing number!");
+else if (miNumber === 9) console.log("9 is an amazing number!");
+else console.log("The number is not 23 or 7!");
+
+if (String(miNumber) != 23) console.log("Why not 23?");
+
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+let shouldDrive = hasDriversLicense && hasGoodVision;
+if (shouldDrive) console.log("Sarah is able to drive!");
+else console.log("Someone else should drive...");
+
+const isTired = true; // C
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+shouldDrive = hasDriversLicense && hasGoodVision && !isTired;
+if (shouldDrive) console.log("Sarah is able to drive!");
+
 */
